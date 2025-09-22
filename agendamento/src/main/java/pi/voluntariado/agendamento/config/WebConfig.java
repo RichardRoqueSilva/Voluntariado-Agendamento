ad59ed89b38aa9b217ff7a39d83b09829694fea4
ViniciusHP <viniciushp97@gmail.com>
2025-09-22 12:16:51 -0300

    [TASK] - Adicionadas as origens permitidas em variável de ambiente

--- agendamento/src/main/java/pi/voluntariado/agendamento/config/WebConfig.java
+++ agendamento/src/main/java/pi/voluntariado/agendamento/config/WebConfig.java
@@ -1,16 +1,27 @@
 package pi.voluntariado.agendamento.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @Configuration
+@Slf4j
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${voluntariado.origenspermitidas}")
+    private List<String> origensPermitidas;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        log.info("Origens permitidas: {}", String.join(",", origensPermitidas));
         registry.addMapping("/**") // Permite CORS para todos os endpoints
-                .allowedOrigins("http://localhost:4200", "http://127.0.0.1:4200") // Permite requisições do Angular em desenvolvimento
+                .allowedOrigins(origensPermitidas.toArray(new String[]{})) // Permite requisições do Angular em desenvolvimento
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT") // Métodos permitidos
                 .allowedHeaders("*") // Permite todos os cabeçalhos
                 .allowCredentials(true); // Permite o envio de cookies/cabeçalhos de autenticação
